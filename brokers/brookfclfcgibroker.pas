15f0141347c374664554bb4e60d43a95cf7673aa
Silvio Clecio <silvioprog@gmail.com>
2013-01-01 17:33:54 -0200

    # Please see issue #6 (https://github.com/silvioprog/brookframework/issues/6).

--- brokers/brookfclfcgibroker.pas
+++ brokers/brookfclfcgibroker.pas
@@ -238,10 +238,15 @@ end;
 
 procedure TBrookFCGIHandler.HandleRequest(ARequest: TRequest; AResponse: TResponse);
 begin
-  AResponse.ContentType := FormatContentType;
-  if BrookSettings.Language <> BROOK_DEFAULT_LANGUAGE then
-    TBrookMessages.Service.SetLanguage(BrookSettings.Language);
-  TBrookRouter.Service.Route(ARequest, AResponse);
+  try
+    AResponse.ContentType := FormatContentType;
+    if BrookSettings.Language <> BROOK_DEFAULT_LANGUAGE then
+      TBrookMessages.Service.SetLanguage(BrookSettings.Language);
+    TBrookRouter.Service.Route(ARequest, AResponse);
+  except
+    on E: Exception do
+      ShowRequestException(AResponse, E);
+  end;
 end;
 
 procedure TBrookFCGIHandler.ShowRequestException(R: TResponse; E: Exception);
