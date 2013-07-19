91ae3bfe536089f4a73a78170a051b6f463bc4a0
Silvio Clecio <silvioprog@gmail.com>
2013-07-18 21:11:25 -0300

    # Improves Kendo demo.

--- demos/db/kendoui/src/kendorestactions.pas
+++ demos/db/kendoui/src/kendorestactions.pas
@@ -5,12 +5,13 @@ unit KendoRESTActions;
 interface
 
 uses
-  BrookDBAction, BrookRESTActions, FPJSON;
+  BrookDBAction, BrookRESTActions, BrookRequestHelper, BrookJSONHelper,
+  BrookDBConsts, HTTPDefs, FPJSON;
 
 type
   TKendoRetrieveAction = class(TBrookDBAction)
   public
-    procedure Get; override;
+    procedure Request(ARequest: TRequest; AResponse: TResponse); override;
   end;
 
   TKendoCreateAction = class(TBrookCreateAction)
@@ -24,22 +25,32 @@ type
 
 implementation
 
-procedure TKendoRetrieveAction.Get;
+procedure TKendoRetrieveAction.Request(ARequest: TRequest; AResponse: TResponse);
 var
   VData: TJSONObject;
 begin
-  VData := TJSONObject.Create;
-  try
-    Table.Select('count(*) as items').Open;
-    VData.Add('count', Table.Field('items').AsInteger);
-    Table.Clear.OrderBy('id limit :limit offset :offset').Prepare;
-    Table.Param('limit').AsInteger := Params['take'].AsInteger;
-    Table.Param('offset').AsInteger := Params['skip'].AsInteger;
-    VData.Add('items', Table.Open.Rows);
-    Write(VData.AsJSON);
-  finally
-    VData.Free;
-  end;
+  if ARequest.IsGet and Params.Exists('take') and Params.Exists('skip') then
+  begin
+    VData := TJSONObject.Create;
+    try
+      Table.Select('count(*) as items').Open;
+      VData.Add('count', Table.Field('items').AsInteger);
+      Table.Clear.OrderBy(BROOK_DEFAULT_KEY_NAME +
+        ' limit :take offset :skip');
+      if Values.Count > 0 then
+        Table.CreateFields(Values).Conditions(Values).Prepare.Bind(Values)
+      else
+        Table.Prepare;
+      Table.Param('take').AsInteger := Params['take'].AsInteger;
+      Table.Param('skip').AsInteger := Params['skip'].AsInteger;
+      VData.Add('items', Table.Open.Rows);
+      Write(VData.AsJSON);
+    finally
+      VData.Free;
+    end;
+  end
+  else
+    inherited Request(ARequest, AResponse);
 end;
 
 end.
