2e079ef84d8684695b323ff89dc276b211682302
Silvio Clecio <silvioprog@gmail.com>
2013-08-04 16:25:26 -0300

    # Updates the broker unit of all demos.

--- demos/http/transferencodingchunked/brokers.pas
+++ demos/http/transferencodingchunked/brokers.pas
@@ -5,17 +5,8 @@ unit Brokers;
 interface
 
 uses
-  BrookFCLCGIBroker, BrookUtils;
+  BrookFCLCGIBroker;
 
 implementation
-
-initialization
-  BrookSettings.Page404 :=
-    '<html><head><title>Page not found</title></head><body>' +
-    '<h1>404 - Page not found</h1></body></html>';
-  BrookSettings.Page500 :=
-    '<html><head><title>Internal server error</title></head><body>' +
-    '<h1>500 - Internal server error</h1>' +
-    '<p style="color: red;" >Error: @error</p></body></html>';
 
 end.
