9730c6240e6f1ed81e428d8a7798b8a6f923911c
Silvio Clecio <silvioprog@gmail.com>
2013-10-14 01:16:58 -0300

    # Clean up.

--- demos/db/blog/src/brokers.pas
+++ demos/db/blog/src/brokers.pas
@@ -5,19 +5,19 @@ unit Brokers;
 interface
 
 uses
-  BrookFCLHTTPAppBroker, BrookUtils, BrookHTTPConsts, BrookSQLdbBroker,
-  SQLite3Conn, BrookApplication;
+  BrookFCLCGIBroker, BrookUtils, BrookHTTPConsts, BrookSQLdbBroker, SQLite3Conn,
+  BrookApplication;
 
 const
-  PUBLIC_HTML = '/media/Sources/lazarus-fpc/git/brookframework/demos/db/blog/html/';
+  PUBLIC_HTML ={$IFDEF MSWINDOWS}'C:\websrv\htdocs\'{$ENDIF}'/var/www/'{$ELSE};
 
 implementation
 
 initialization
-  BrookSettings.Port := 2015;
   BrookSettings.Configuration := 'db.cfg';
   BrookSettings.Charset := BROOK_HTTP_CHARSET_UTF_8;
   BrookSettings.Page404File := PUBLIC_HTML + '404.html';
   BrookSettings.Page500File := PUBLIC_HTML + '500.html';
-  BrookSettings.RootUrl := 'http://localhost:2015';
+  BrookSettings.RootUrl := 'http://localhost/cgi-bin/blog';
+
 end.
