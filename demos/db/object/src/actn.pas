2fa6f0a18deddfa8f7e52531105cb6a143c18cd5
silvioprog <silvioprog@gmail.com>
2013-11-05 02:10:41 -0200

    # Clean up.

--- demos/db/object/src/actn.pas
+++ demos/db/object/src/actn.pas
@@ -5,7 +5,7 @@ unit Actn;
 interface
 
 uses
-  BrookDBAction, SysUtils, FPJSON, JSONRTTIUtils, Person;
+  BrookDBAction, BrookUtils, SysUtils, FPJSON, Person;
 
 type
   TPersonAction = class(TBrookDBAction)
@@ -29,14 +29,14 @@ begin
       VPerson.Weight := 6.3;
       VPerson.LastUpdate := Now;
       VPerson.Validate;
-      VData := ObjectToJSON(VPerson);
+      VData := BrookObjectToJson(VPerson);
       Table.Insert(VData).Apply;
       Write('Please use F5 to show the inserted object ...');
     end
     else
     begin
       Table.First.GetRow(VData);
-      JSONToObject(VData, VPerson);
+      BrookJsonToObject(VData, VPerson);
       Write('Active: %s, Name: %s, Age: %d, Weight: %f, LastUpdate: %s', [
         BoolToStr(VPerson.Active, 'Y', 'N'), VPerson.Name, VPerson.Age,
         VPerson.Weight, FormatDateTime('yyyy/mm/dd hh:nn:ss',
