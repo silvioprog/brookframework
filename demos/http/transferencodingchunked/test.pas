9e6c534b913a1a4f533462ed5d952f086dea3196
silvioprog <silvioprog@gmail.com>
2013-12-27 17:33:30 -0200

    # Clean up.

--- demos/http/transferencodingchunked/test.pas
+++ demos/http/transferencodingchunked/test.pas
@@ -7,21 +7,20 @@ unit Test;
 interface
 
 uses
-  BrookAction, BrookHTTPConsts, HTTPDefs;
+  BrookAction, BrookHTTPConsts;
 
 type
   TTest = class(TBrookAction)
   public
-    procedure Request({%H-}ARequest: TRequest;
-      {%H-}AResponse: TResponse); override;
+    procedure Get; override;
   end;
 
 implementation
 
-procedure TTest.Request(ARequest: TRequest; AResponse: TResponse);
+procedure TTest.Get;
 begin
-  AResponse.ContentType := BROOK_HTTP_CONTENT_TYPE_TEXT_PLAIN;
-  AResponse.SetCustomHeader(BROOK_HTTP_HEADER_TRANSFER_ENCODING,
+  GetResponse.ContentType := BROOK_HTTP_CONTENT_TYPE_TEXT_PLAIN;
+  GetResponse.SetCustomHeader(BROOK_HTTP_HEADER_TRANSFER_ENCODING,
     BROOK_HTTP_TRANSFER_ENCODING_CHUNKED);
   Write('23');
   Write('This is the data in the first chunk');
