4d82daedc356a7abf5ca359612715035387dc98b
silvioprog <silvioprog@gmail.com>
2014-06-27 15:00:51 -0300

    Added the designtime package.

---
+++ design/brookregallcomp.pas
@@ -0,0 +1,32 @@
+(*
+  Brook framework, Register all Components
+
+  Copyright (C) 2014 Silvio Clecio
+
+  See the file LICENSE.txt, included in this distribution,
+  for details about the copyright.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*)
+
+unit BrookRegAllComp;
+
+{$i brook.inc}
+
+interface
+
+uses
+  BrookRouterHandler, Classes;
+
+procedure Register;
+
+implementation
+
+procedure Register;
+begin
+  RegisterComponents('Brook framework', [TBrookRouterHandler]);
+end;
+
+end.
