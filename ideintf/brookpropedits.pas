50e16040d282b9147dc268bbd7792f4bbc370e91
silvioprog <silvioprog@gmail.com>
2014-06-27 16:07:56 -0300

    Added the property editors unit.

---
+++ ideintf/brookpropedits.pas
@@ -0,0 +1,29 @@
+(*
+  Brook framework, Property Editors Unit
+
+  Copyright (C) 2014 Silvio Clecio
+
+  See the file LICENSE.txt, included in this distribution,
+  for details about the copyright.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*)
+
+unit BrookPropEdits;
+
+{$i brook.inc}
+
+interface
+
+uses
+  BrookSession, PropEdits;
+
+implementation
+
+initialization
+  RegisterPropertyEditor(TypeInfo(AnsiString), TBrookSession, 'Directory',
+    TDirectoryPropertyEditor);
+
+end.
