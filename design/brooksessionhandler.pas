77bd63a93db8380eb048e863dd001a366910803c
silvioprog <silvioprog@gmail.com>
2014-06-27 17:55:13 -0300

    Clean up.

--- design/brooksessionhandler.pas
+++ design/brooksessionhandler.pas
@@ -24,28 +24,16 @@ type
   { Handles the session features. }
   TBrookSessionHandler = class(TBrookSession)
   published
-    { Set the session cookie name. }
     property CookieName;
-    { Set the session cookie domain. }
     property CookieDomain;
-    { Set the session cookie path. }
     property CookiePath;
-    { Set the session cookie secure. }
     property CookieSecure;
-    { Set the session cookie expiration. }
     property CookieExpires;
-    { The session fields. }
     property Fields;
-    { The ignored fields by the session. }
     property IgnoredFields;
-    { Set the name of session directory. }
     property Directory;
-    { The session file prefix. }
     property FilePrefix;
-    { The remaining seconds for the session finish. }
     property TimeOut;
-    { Informs if the session cookie is accessible only by HTTP requests,
-      if @code(True), the JavaScript access is not allowed. }
     property HttpOnly;
   end;
 
