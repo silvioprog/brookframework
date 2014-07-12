24afc5702493816078e199bc6d1572fe54ba6ad3
silvioprog <silvioprog@gmail.com>
2014-07-12 14:46:16 -0300

    Added the resource strings.

--- core/brookmessages.pas
+++ core/brookmessages.pas
@@ -17,34 +17,63 @@ unit BrookMessages;
 
 interface
 
+resourcestring
+  { Default resource messages }
+  { }
+  SBrookDefaultLocale_rst = 'en-US';
+  { Default resource error messages }
+  { }
+  SBrookInvalidRequestMethodError_rst = 'Invalid request method: %s';
+  SBrookItemNotFoundError_rst = 'Item "%s" not found.';
+  SBrookFileNotFoundError_rst = 'File not found: %s';
+  SBrookNoRequestMethodError_rst = 'No REQUEST_METHOD passed from server.';
+  SBrookNoApplicationRegisteredError_rst = 'No application registered.';
+  SBrookApplicationAlreadyRegisteredError_rst = 'The application is already registered.';
+  SBrookRegiterTBrookActionError_rst = 'Unable to register the type TBrookAction directly.';
+  SBrookActionAlreadyRegisteredError_rst = 'The action "%s" is already registered.';
+  SBrookDefaultActionAlreadyRegisteredError_rst = 'There is already a default action registered.';
+  SBrookPatternAlreadyRegisteredError_rst = 'There is already an action registered with the pattern "%s".';
+  SBrookRouterServiceAlreadyRegisteredError_rst = 'The router service is already registered.';
+  SBrookNoRouterServiceRegisteredError_rst = 'No router service registered.';
+  SBrookNoRouteRegisteredError_rst = 'No route registered.';
+  SBrookCfgFileNotFoundError_rst = 'The config file was not found: "%s"';
+  SBrookNotNilError_rst = '"%s" must not be nil.';
+  SBrookEmptyLibraryNameError_rst = 'The library name must not be empty.';
+  SBrookMethodNotAllowedError_rst = 'HTTP method not allowed for the requested resource.';
+  SBrookConstraintAlreadyRegisteredError_rst = 'The constraint "%s" is already registered.';
+  SBrookConstraintsServiceAlreadyRegisteredError_rst = 'The constraints service is already registered.';
+  SBrookNoConstraintsServiceRegisteredError_rst = 'No constraints service registered.';
+  SBrookNoLoggerServiceRegisteredError_rst = 'No logger service registered.';
+  SBrookLoggerServiceAlreadyRegisteredError_rst = 'The logger service is already registered.';
+
 var
-  { Default msgs }
+  { Default messages }
   { }
-  SBrookDefaultLocale: string = 'en-US';
-  { Error msgs }
+  SBrookDefaultLocale: string = SBrookDefaultLocale_rst;
+  { Default error messages }
   { }
-  SBrookInvalidRequestMethodError: string = 'Invalid request method: %s';
-  SBrookItemNotFoundError: string = 'Item "%s" not found.';
-  SBrookFileNotFoundError: string = 'File not found: %s';
-  SBrookNoRequestMethodError: string = 'No REQUEST_METHOD passed from server.';
-  SBrookNoApplicationRegisteredError: string = 'No application registered.';
-  SBrookApplicationAlreadyRegisteredError: string = 'The application is already registered.';
-  SBrookRegiterTBrookActionError: string = 'Unable to register the type TBrookAction directly.';
-  SBrookActionAlreadyRegisteredError: string = 'The action "%s" is already registered.';
-  SBrookDefaultActionAlreadyRegisteredError: string = 'There is already a default action registered.';
-  SBrookPatternAlreadyRegisteredError: string = 'There is already an action registered with the pattern "%s".';
-  SBrookRouterServiceAlreadyRegisteredError: string = 'The router service is already registered.';
-  SBrookNoRouterServiceRegisteredError: string = 'No router service registered.';
-  SBrookNoRouteRegisteredError: string = 'No route registered.';
-  SBrookCfgFileNotFoundError: string = 'The config file was not found: "%s"';
-  SBrookNotNilError: string = '"%s" must not be nil.';
-  SBrookEmptyLibraryNameError: string = 'The library name must not be empty.';
-  SBrookMethodNotAllowedError: string = 'HTTP method not allowed for the requested resource.';
-  SBrookConstraintAlreadyRegisteredError: string = 'The constraint "%s" is already registered.';
-  SBrookConstraintsServiceAlreadyRegisteredError: string = 'The constraints service is already registered.';
-  SBrookNoConstraintsServiceRegisteredError: string = 'No constraints service registered.';
-  SBrookNoLoggerServiceRegisteredError: string = 'No logger service registered.';
-  SBrookLoggerServiceAlreadyRegisteredError: string = 'The logger service is already registered.';
+  SBrookInvalidRequestMethodError: string = SBrookInvalidRequestMethodError_rst;
+  SBrookItemNotFoundError: string = SBrookItemNotFoundError_rst;
+  SBrookFileNotFoundError: string = SBrookFileNotFoundError_rst;
+  SBrookNoRequestMethodError: string = SBrookNoRequestMethodError_rst;
+  SBrookNoApplicationRegisteredError: string = SBrookNoApplicationRegisteredError_rst;
+  SBrookApplicationAlreadyRegisteredError: string = SBrookApplicationAlreadyRegisteredError_rst;
+  SBrookRegiterTBrookActionError: string = SBrookRegiterTBrookActionError_rst;
+  SBrookActionAlreadyRegisteredError: string = SBrookActionAlreadyRegisteredError_rst;
+  SBrookDefaultActionAlreadyRegisteredError: string = SBrookDefaultActionAlreadyRegisteredError_rst;
+  SBrookPatternAlreadyRegisteredError: string = SBrookPatternAlreadyRegisteredError_rst;
+  SBrookRouterServiceAlreadyRegisteredError: string = SBrookRouterServiceAlreadyRegisteredError_rst;
+  SBrookNoRouterServiceRegisteredError: string = SBrookNoRouterServiceRegisteredError_rst;
+  SBrookNoRouteRegisteredError: string = SBrookNoRouteRegisteredError_rst;
+  SBrookCfgFileNotFoundError: string = SBrookCfgFileNotFoundError_rst;
+  SBrookNotNilError: string = SBrookNotNilError_rst;
+  SBrookEmptyLibraryNameError: string = SBrookEmptyLibraryNameError_rst;
+  SBrookMethodNotAllowedError: string = SBrookMethodNotAllowedError_rst;
+  SBrookConstraintAlreadyRegisteredError: string = SBrookConstraintAlreadyRegisteredError_rst;
+  SBrookConstraintsServiceAlreadyRegisteredError: string = SBrookConstraintsServiceAlreadyRegisteredError_rst;
+  SBrookNoConstraintsServiceRegisteredError: string = SBrookNoConstraintsServiceRegisteredError_rst;
+  SBrookNoLoggerServiceRegisteredError: string = SBrookNoLoggerServiceRegisteredError_rst;
+  SBrookLoggerServiceAlreadyRegisteredError: string = SBrookLoggerServiceAlreadyRegisteredError_rst;
 
 implementation
 
