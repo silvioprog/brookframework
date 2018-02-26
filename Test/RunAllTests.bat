51b92f61efb0511519d79e02ec3177d8e4e6099c
silvioprog <silvioprog@gmail.com>
2018-02-26 03:22:25 -0300

    [test] adds batch script that runs all tests on Windows.

---
+++ Test/RunAllTests.bat
@@ -0,0 +1,37 @@
+::     _____   _____    _____   _____   _   __
+::    |  _  \ |  _  \  /  _  \ /  _  \ | | / /
+::    | |_) | | |_) |  | | | | | | | | | |/ /
+::    |  _ <  |  _ <   | | | | | | | | |   (
+::    | |_) | | | \ \  | |_| | | |_| | | |\ \
+::    |_____/ |_|  \_\ \_____/ \_____/ |_| \_\
+:: 
+::    –– a small library which helps you write quickly REST APIs.
+:: 
+::  Copyright (c) 2012-2018 Silvio Clecio <silvioprog@gmail.com>
+:: 
+::  This file is part of Brook library.
+:: 
+::  Brook library is free software: you can redistribute it and/or modify
+::  it under the terms of the GNU Lesser General Public License as published by
+::  the Free Software Foundation, either version 3 of the License, or
+::  (at your option) any later version.
+:: 
+::  Brook library is distributed in the hope that it will be useful,
+::  but WITHOUT ANY WARRANTY; without even the implied warranty of
+::  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+::  GNU Lesser General Public License for more details.
+:: 
+::  You should have received a copy of the GNU Lesser General Public License
+::  along with Brook library.  If not, see <http://www.gnu.org/licenses/>.
+::
+
+@ECHO OFF
+SET /A COUNT = 0
+FOR /R %%T IN (*.exe) DO (
+    ECHO | SET /P = "Running test %%~NXT "
+    CMD /C %%T || GOTO :ERROR
+    ECHO OK
+    SET /A COUNT += 1
+)
+ECHO Total of ran tests: %COUNT%
+:ERROR
