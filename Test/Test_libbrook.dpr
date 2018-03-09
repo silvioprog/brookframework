fa175c1feab1e6a8241c91d2656dfadca0e34ce9
silvioprog <silvioprog@gmail.com>
2018-03-09 01:04:29 -0300

    [test] adds the test for the binding.

---
+++ Test/Test_libbrook.dpr
@@ -0,0 +1,42 @@
+program Test_libbrook;
+
+{$I Tests.inc}
+
+uses
+  SysUtils,
+  libbrook;
+
+procedure Test_LoadLibrary;
+begin
+  BkUnloadLibrary;
+  Assert(BkLoadLibrary('') = NilHandle);
+  Assert(BkLoadLibrary('abc') = NilHandle);
+  Assert(BkLoadLibrary(BK_LIB_NAME) <> NilHandle);
+end;
+
+procedure Test_UnloadLibrary;
+begin
+  Assert(BkLoadLibrary(BK_LIB_NAME) <> NilHandle);
+  Assert(BkUnloadLibrary = NilHandle);
+end;
+
+procedure Test_CheckLibrary;
+var
+  OK: Boolean;
+begin
+  OK := False;
+  try
+    BkUnloadLibrary;
+    BkCheckLibrary;
+  except
+    on E: Exception do
+      OK := E.ClassType = EBkLibraryNotLoaded;
+  end;
+  Assert(OK);
+end;
+
+begin
+  Test_LoadLibrary;
+  Test_UnloadLibrary;
+  Test_CheckLibrary;
+end.
