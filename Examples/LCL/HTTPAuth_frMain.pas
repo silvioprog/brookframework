4adac8effcf702a15fc73a85c8f1158f6288042c
silvioprog <silvioprog@gmail.com>
2018-08-10 01:42:54 -0300

    credits upgrade

--- Examples/LCL/HTTPAuth_frMain.pas
+++ Examples/LCL/HTTPAuth_frMain.pas
@@ -1,28 +1,27 @@
-(*    _____   _____    _____   _____   _   __
- *   |  _  \ |  _  \  /  _  \ /  _  \ | | / /
- *   | |_) | | |_) |  | | | | | | | | | |/ /
- *   |  _ <  |  _ <   | | | | | | | | |   (
- *   | |_) | | | \ \  | |_| | | |_| | | |\ \
- *   |_____/ |_|  \_\ \_____/ \_____/ |_| \_\
+(*   _                     _
+ *  | |__  _ __ ___   ___ | | __
+ *  | '_ \| '__/ _ \ / _ \| |/ /
+ *  | |_) | | | (_) | (_) |   <
+ *  |_.__/|_|  \___/ \___/|_|\_\
  *
- *   –– a small library which helps you write quickly REST APIs.
+ *  –– an ideal Pascal microframework to develop cross-platform HTTP servers.
  *
  * Copyright (c) 2012-2018 Silvio Clecio <silvioprog@gmail.com>
  *
  * This file is part of Brook library.
  *
- * Brook library is free software: you can redistribute it and/or modify
+ * Brook framework is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Brook library is distributed in the hope that it will be useful,
+ * Brook framework is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with Brook library.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Brook framework.  If not, see <http://www.gnu.org/licenses/>.
  *)
 
 unit HTTPAuth_frMain;
