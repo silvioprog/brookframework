76c9759efdab5e282f3acbada3b1e9129020025b
silvioprog <silvioprog@gmail.com>
2018-09-03 20:00:15 -0300

    Implements an experimental base structure for the routing feature.

--- Source/BrookRouter.pas
+++ Source/BrookRouter.pas
@@ -31,10 +31,19 @@ unit BrookRouter;
 interface
 
 uses
+  SysUtils,
   Classes,
+  Platform,
+  Marshalling,
   libsagui,
+  BrookUtils,
   BrookHandledClasses,
   BrookRoutes;
+
+resourcestring
+  SBrookOpNotAllowedInactiveRouter =
+    'Operation is not allowed while the router is inactive.';
+  SBrookCannotCreateRouterHandle = 'Cannot create router handle.';
 
 type
   TBrookRouter = class;
@@ -42,20 +51,46 @@ type
   TBrookRouterCreatePanelClassEvent = procedure(ASender: TBrookRouter;
     var ARouteClass: TBrookRouteClass) of object;
 
+  EBrookRouter = class(Exception);
+
+  EBrookOpNotAllowedInactiveRouter = class(Exception);
+
   TBrookRouter = class(TBrookHandledComponent)
   private
     FRoutes: TBrookRoutes;
+    FHandle: Psg_router;
+    FActive: Boolean;
+    FStreamedActive: Boolean;
     FOnCreateRouteClass: TBrookRouterCreatePanelClassEvent;
+    FOnError: TBrookErrorEvent;
+    function IsActive: Boolean;
+    procedure SetActive(AValue: Boolean);
     procedure SetRoutes(AValue: TBrookRoutes);
+    procedure InternalCreateRouterHandle;
+    procedure InternalFreeRouterHandle; inline;
   protected
+    class procedure DoErrorCallback(Acls: Pcvoid;
+      const Aerr: Pcchar); cdecl; static;
     function CreateRoutes: TBrookRoutes; virtual;
+    procedure Loaded; override;
+    function GetHandle: Pointer; override;
+    procedure DoError(ASender: TObject; AException: Exception); virtual;
     function GetRouteClass: TBrookRouteClass; virtual;
+    procedure DoOpen; virtual;
+    procedure DoClose; virtual;
+    procedure CheckActive; inline;
   public
     constructor Create(AOwner: TComponent); override;
+    destructor Destroy; override;
+    procedure Open;
+    procedure Close;
+    function Route(const APath: string; AUserData: Pointer): Boolean; virtual;
   published
+    property Active: Boolean read FActive write SetActive stored IsActive;
     property Routes: TBrookRoutes read FRoutes write SetRoutes;
     property OnCreateRouteClass: TBrookRouterCreatePanelClassEvent read
       FOnCreateRouteClass write FOnCreateRouteClass;
+    property OnError: TBrookErrorEvent read FOnError write FOnError;
   end;
 
 implementation
@@ -66,9 +101,77 @@ begin
   FRoutes := CreateRoutes;
 end;
 
+destructor TBrookRouter.Destroy;
+begin
+  FRoutes.Free;
+  try
+    SetActive(False);
+  finally
+    inherited Destroy;
+  end;
+end;
+
+procedure TBrookRouter.InternalCreateRouterHandle;
+begin
+  FHandle := sg_router_new2(FRoutes.Handle,
+{$IFNDEF VER3_0}@{$ENDIF}DoErrorCallback, Self);
+  if not Assigned(FHandle) then
+    raise EInvalidPointer.CreateRes(@SBrookCannotCreateRouterHandle);
+end;
+
+procedure TBrookRouter.InternalFreeRouterHandle;
+begin
+  sg_router_free(FHandle);
+  FHandle := nil;
+end;
+
+class procedure TBrookRouter.DoErrorCallback(Acls: Pcvoid; const Aerr: Pcchar);
+  cdecl;
+var
+  VRouter: TBrookRouter absolute Acls;
+  VException: EBrookRouter;
+begin
+  VException := EBrookRouter.Create(TMarshal.ToString(Aerr));
+  try
+    VRouter.DoError(VRouter, VException);
+  finally
+    VException.Free;
+  end;
+end;
+
 function TBrookRouter.CreateRoutes: TBrookRoutes;
 begin
   Result := TBrookRoutes.Create(Self);
+end;
+
+procedure TBrookRouter.Loaded;
+begin
+  inherited Loaded;
+  try
+    if FStreamedActive then
+      SetActive(True);
+  except
+    if csDesigning in ComponentState then
+    begin
+      if Assigned(ApplicationHandleException) then
+        ApplicationHandleException(ExceptObject)
+      else
+        ShowException(ExceptObject, ExceptAddr);
+    end
+    else
+      raise;
+  end;
+end;
+
+function TBrookRouter.GetHandle: Pointer;
+begin
+  Result := FHandle;
+end;
+
+procedure TBrookRouter.DoError(ASender: TObject; AException: Exception);
+begin
+  if Assigned(FOnError) then
+    FOnError(ASender, AException);
 end;
 
 function TBrookRouter.GetRouteClass: TBrookRouteClass;
@@ -79,6 +182,82 @@ end;
 procedure TBrookRouter.SetRoutes(AValue: TBrookRoutes);
 begin
   FRoutes.Assign(AValue);
+end;
+
+function TBrookRouter.IsActive: Boolean;
+begin
+  Result := FActive;
+end;
+
+procedure TBrookRouter.SetActive(AValue: Boolean);
+begin
+  if AValue = FActive then
+    Exit;
+  if csDesigning in ComponentState then
+  begin
+    if not (csLoading in ComponentState) then
+      SgCheckLibrary;
+    FActive := AValue;
+  end
+  else
+    if AValue then
+    begin
+      if csReading in ComponentState then
+        FStreamedActive := True
+      else
+        DoOpen;
+    end
+    else
+      DoClose;
+end;
+
+procedure TBrookRouter.DoOpen;
+begin
+  if Assigned(FHandle) then
+    Exit;
+  SgCheckLibrary;
+  InternalCreateRouterHandle;
+  if not FActive then
+    InternalFreeRouterHandle;
+end;
+
+procedure TBrookRouter.DoClose;
+begin
+  if not Assigned(FHandle) then
+    Exit;
+  SgCheckLibrary;
+  InternalFreeRouterHandle;
+  FActive := False;
+end;
+
+procedure TBrookRouter.Open;
+begin
+  SetActive(True);
+end;
+
+procedure TBrookRouter.Close;
+begin
+  SetActive(False);
+end;
+
+procedure TBrookRouter.CheckActive;
+begin
+  if (not (csLoading in ComponentState)) and (not Active) then
+    raise EBrookOpNotAllowedInactiveRouter.CreateRes(
+      @SBrookOpNotAllowedInactiveRouter);
+end;
+
+function TBrookRouter.Route(const APath: string; AUserData: Pointer): Boolean;
+var
+  M: TMarshaller;
+  R: cint;
+begin
+  CheckActive;
+  SgCheckLibrary;
+  R := sg_router_dispatch(FHandle, M.ToCNullable(APath), AUserData);
+  Result := R = 0;
+  if (not Result) and (R <> ENOENT) then
+    SgCheckLastError(R);
 end;
 
 end.
