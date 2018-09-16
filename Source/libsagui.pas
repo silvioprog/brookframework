silvioprog/brookframework

commit 4f0241f25f305fcede7b173d35a0719f3d3b9030

Cosmetic

Source/libsagui.pas
@@ -31,6 +31,7 @@ unit libsagui;
 
 {$IFDEF FPC}
  {$MODE DELPHI}
+ {$PACKRECORDS C}
  {$IFDEF VER3_0}
   {$PUSH}{$MACRO ON}
   {$DEFINE MarshaledAString := PAnsiChar}
@@ -46,15 +47,11 @@ interface
 uses
   SysUtils,
   StrUtils,
+{$IFDEF MSWINDOWS}
+ {$IFNDEF FPC}Winapi.{$ENDIF}Windows,
+{$ENDIF}
 {$IFDEF FPC}
- {$IFDEF MSWINDOWS}
-  Windows,
- {$ENDIF}
   DynLibs,
-{$ELSE}
- {$IFDEF MSWINDOWS}
-  Winapi.Windows,
- {$ENDIF}
 {$ENDIF}
   SyncObjs;
 
@@ -62,9 +59,11 @@ const
 {$IFDEF FPC}
  {$IFDEF VER3_0}
   NilHandle = DynLibs.NilHandle;
+  TLibHandle = DynLibs.TLibHandle;
  {$ENDIF}
 {$ELSE}
   NilHandle = HMODULE(0);
+  TLibHandle = HMODULE;
 {$ENDIF}
 
 {$IF (NOT DEFINED(FPC)) OR DEFINED(VER3_0)}
@@ -109,15 +108,6 @@ type
   PPcvoid = PPointer;
   cenum = cint;
   cva_list = Pointer;
-
-{$IFDEF FPC}
- {$PACKRECORDS C}
- {$IFDEF VER3_0}
-  TLibHandle = DynLibs.TLibHandle;
- {$ENDIF}
-{$ELSE}
-  TLibHandle = HMODULE;
-{$ENDIF}
 
   sg_err_cb = procedure(cls: Pcvoid; const err: Pcchar); cdecl;
 

commit 98dd9610e2cbf9962b3eb5e77831d1b2f5f5d667

Fix build broken by 4f0241f25f305fcede7b173d35a0719f3d3b9030.

Source/libsagui.pas
@@ -48,24 +48,28 @@ uses
   SysUtils,
   StrUtils,
 {$IFDEF MSWINDOWS}
- {$IFNDEF FPC}Winapi.{$ENDIF}Windows,
+  Windows,
 {$ENDIF}
 {$IFDEF FPC}
   DynLibs,
 {$ENDIF}
   SyncObjs;
 
-const
 {$IFDEF FPC}
  {$IFDEF VER3_0}
+const
   NilHandle = DynLibs.NilHandle;
+type
   TLibHandle = DynLibs.TLibHandle;
  {$ENDIF}
 {$ELSE}
+const
   NilHandle = HMODULE(0);
+type
   TLibHandle = HMODULE;
 {$ENDIF}
 
+const
 {$IF (NOT DEFINED(FPC)) OR DEFINED(VER3_0)}
   SharedSuffix =
  {$IF DEFINED(MSWINDOWS)}